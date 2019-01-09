0c49ed6433d3a764f5411b1eab016e2238e108b2
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2019-01-09 15:54:53 -0300

    Footer with contact form, some changes in top header also

--- app/components/Forms/Contact.js
+++ app/components/Forms/Contact.js
@@ -3,77 +3,79 @@ import PropTypes from 'prop-types';
 
 /* eslint-disable global-require */
 class Contact extends Component {
-  state = {
+    state = {
         fullname: "",
-    mail: '',
+        mail: '',
         message: "",
     };
 
-  handleChangeName(event) {
-    this.setState({ fullname: event.target.value });
-  }
+    handleChangeName(event) {
+        this.setState({ fullname: event.target.value });
+    }
 
-  handleChangeEmail(event) {
+    handleChangeEmail(event) {
         this.setState({ mail: event.target.value });
-  }
+    }
 
-  handleChangeMessage(event) {
+    handleChangeMessage(event) {
         this.setState({ message: event.target.value });
-  }
+    }
 
-  submit(event) {
+    submit(event) {
         console.log("Submit");
     }
 
-  render() {
+    render() {
         const { classes } = this.props;
-    return (
+        return (
             <div style={classes.container}>
-        <div>
+                <div>
                     <div>
                         Full Name
                     </div>
                     <div>
-            <input
-              style={classes.input}
-              type="text"
-              value={this.state.fullname}
-              onChange={this.handleChangeName.bind(this)}
-            </div>
+                        <input
+                            style={classes.input}
+                            type="text"
+                            value={this.state.fullname}
+                            onChange={this.handleChangeName.bind(this)} />
+                    </div>
                 </div>
                 <div>
                     <div>
                         Email
                     </div>
                     <div>
-            <input
-              style={classes.input}
-              type="text"
-              value={this.state.mail}
-              onChange={this.handleChangeEmail.bind(this)}
-            </div>
-          </div>
+                        <input
+                            style={classes.input}
+                            type="text"
+                            value={this.state.mail}
+                            onChange={this.handleChangeEmail.bind(this)} />
+                    </div>
+                </div>
                 <div>
                     <div>
-          <div>Message</div>
+                        <div>Message</div>
+                        <div>
+                            <textarea
+                                style={classes.inputMessage}
+                                value={this.state.message}
+                                onChange={this.handleChangeMessage.bind(this)} />
+                        </div>
+                    </div>
                     <div>
-            <textarea
-              style={classes.inputMessage}
-              value={this.state.message}
-              onChange={this.handleChangeMessage.bind(this)}
-            </div>
+                        <input
+                            style={classes.button}
+                            type="button"
+                            value="Submit"
+                            onClick={this.submit.bind(this)} />
+                    </div>
                 </div>
-        <div>
-          <input
-            style={classes.button}
-            type="button"
-            value="Submit"
-            onClick={this.submit.bind(this)}
-          </div>
             </div>
-    );
+        );
     }
 }
+
 /* eslint-enable global-require */
 Contact.propTypes = {
     classes: PropTypes.object.isRequired,
